6c5f0bf9a491afcf42a6615d1de283a07890aef6
Diego-Morales-30 <diegollavemorales@gmail.com>
2025-10-30 22:23:38 -0500

    Borrando

--- Proyecto_HP2_DATAFILE/DataFile_HP2/DataFile_HP2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DataFile_HP2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
