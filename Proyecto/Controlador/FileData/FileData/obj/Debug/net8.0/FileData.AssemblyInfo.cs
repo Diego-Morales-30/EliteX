a11880e1e00ea2e342915017cf37f6d3f8921246
Diego-Morales-30 <diegollavemorales@gmail.com>
2025-11-06 22:50:55 -0500

    ACtualizacion 3.0 Focus

--- Proyecto/Controlador/FileData/FileData/obj/Debug/net8.0/FileData.AssemblyInfo.cs
+++ Proyecto/Controlador/FileData/FileData/obj/Debug/net8.0/FileData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f747e311762268095dc44a3001e18f561a7117d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f233b56afbb6c81b8d74ba2ce15332032d5c3b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
